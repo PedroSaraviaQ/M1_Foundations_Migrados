84ad2ce577417eb7e6ec1d2555e3835e0c2b08f0
Pedro <pedro.saravia.q@uni.pe>
2022-05-06 15:59:20 -0500

    functions&recursion

--- 03-Functions/03-Functions.js
+++ 03-Functions/03-Functions.js
@@ -1,10 +1,26 @@
-function timeConversion(time){
-// dada la hora en formato hora AM/PM , convertir a la hora militar (24-horas).
-// Recordar: las 12:00:00AM en un reloj de 12 horas son las 00:00:00 en un reloj de 24 horas
-//las 12:00:00PM en un reloj de 12 horas son las 12:00:00 en un reloj de 24 horas 
-
+function timeConversion(time) {
+  // dada la hora en formato hora AM/PM , convertir a la hora militar (24-horas).
+  // Recordar: las 12:00:00AM en un reloj de 12 horas son las 00:00:00 en un reloj de 24 horas
+  //las 12:00:00PM en un reloj de 12 horas son las 12:00:00 en un reloj de 24 horas
+  if (
+    time.slice(0, 2) > 12 ||
+    time.slice(3, 5) > 59 ||
+    time.slice(6, 8) > 59 ||
+    time.length !== 10
+  )
+    return false;
+  let h = time.slice(0, 2);
+  if (time[8] === "A") {
+    if (h === "12") time = time.replace(h, "00");
+    time = time.replace("A", "");
+  } else {
+    if (h !== "12") time = time.replace(h, h * 1 + 12);
+    time = time.replace("P", "");
+  }
+  time = time.replace("M", "");
+  return time;
 }
 
 module.exports = {
-    timeConversion
-}
+  timeConversion,
+};
--- 04-Recursion/04-recursion.js
+++ 04-Recursion/04-recursion.js
@@ -4,29 +4,32 @@
 
 const producto = function (array) {
   //escribe aqui tu codigo
-
+  if (array.length === 1) return array[0];
+  return array.pop() * producto(array);
 };
 
 // Dado un objeto con objetos anidados utilizar la recursión para crear una función
 // que devuelva true o false dependiendo si el objeto tiene o no el valor pasado por parametro
 // ejemplo:
 // let obj = {
-  //    prop2:{value:5}     
-  //     school: {
-    //         hogwarts: {
-      //             headmaster:{
-        //               name: {
-          //                 first: "Albus",
-          //                 last: "Dumbledore"
-          //               }
-          //             }
-          //         }
-          //     } 
+//    prop2:{value:5}
+//     school: {
+//         hogwarts: {
+//             headmaster:{
+//               name: {
+//                 first: "Albus",
+//                 last: "Dumbledore"
+//               }
+//             }
+//         }
+//     }
 
-
-
-const isThere = function (obj, value){
-  //escribe aqui tu codigo 
-
+const isThere = function (obj, value) {
+  //escribe aqui tu codigo
+  if (Object.values(obj).includes(value)) return true;
+  for (let key in obj) {
+    if (typeof obj[key] === "object" && isThere(obj[key], value)) return true;
+  }
+  return false;
 };
 module.exports = { producto, isThere };
