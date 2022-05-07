1f266ca9cc05db77aa6adaeef8dbd0483aa377d6
Pedro <pedro.saravia.q@uni.pe>
2022-05-07 01:51:51 -0500

    trees&algorithms

--- 07-Arboles/07-Arboles.js
+++ 07-Arboles/07-Arboles.js
@@ -1,16 +1,20 @@
-const { BinarySearchTree } = require("../estructuras")
+const { BinarySearchTree } = require("../estructuras");
 
 // En el prototipo de BinarySearchTree crear la funcion search que debe recibir un parametro y buscarlo en el arbol
 // si lo encuentra, debe retornar el nodo. En caso contrario retornar el mensaje correspondiente.
-// Ejemplo      (10)          
+// Ejemplo      (10)
 //             /     \
-//          (7)     (12)       
+//          (7)     (12)
 //         /  \      /  \
-//      (2)  (9)  (11)  (15)   
-BinarySearchTree.prototype.search= function(value){
-   
-}
-
+//      (2)  (9)  (11)  (15)
+BinarySearchTree.prototype.search = function (value) {
+  if (this.value === value) return this.value;
+  if (value < this.value) {
+    if (!this.left) return "no se encontró el valor";
+    return this.left.search(value);
+  } else if (!this.right) return "no se encontró el valor";
+  return this.right.search(value);
+};
 
 // En el prototipo de BinarySearchTree crear la funcion height que debe retornar la altura del mismo (cantidad de niveles)
 // Ejemplo      (10)           ----> nivel 0
@@ -19,11 +23,13 @@ BinarySearchTree.prototype.search= function(value){
 //         /  \      /  \
 //      (2)   (9)  (11)  (15)   ----> nivel 2
 
+BinarySearchTree.prototype.height = function () {
+  if (!this.left && !this.right) return 0;
+  if (!this.left) return 1 + this.right.height();
+  if (!this.right) return 1 + this.left.height();
+  return 1 + Math.max(this.left.height(), this.right.height());
+};
 
-BinarySearchTree.prototype.height= function(){
- 
-}
-
-module.exports={
-    BinarySearchTree
-}
+module.exports = {
+  BinarySearchTree,
+};
--- 08-Algoritmos/08-Algoritmos.js
+++ 08-Algoritmos/08-Algoritmos.js
@@ -1,42 +1,77 @@
-'use strict'
+"use strict";
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  
- 
+  for (let i = 1; i < array.length; i++) {
+    let counter = 0;
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        let aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+        counter++;
+      }
+    }
+    if (!counter) break;
+  }
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  
-
+  for (let i = 1; i < array.length; i++) {
+    let aux = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
- 
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) min = j;
+    }
+    if (min !== i) {
+      let aux = array[min];
+      array[min] = array[i];
+      array[i] = aux;
+    }
+  }
+  return array;
 }
-
 
 function quickSort(array) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  
-
-
+  if (array.length <= 1) return array;
+  let pivot = array[Math.floor(Math.random() * array.length)];
+  let left = [];
+  let right = [];
+  let equals = [];
+  array.forEach((e) => {
+    if (e < pivot) left.push(e);
+    else if (e > pivot) right.push(e);
+    else equals.push(e);
+  });
+  return quickSort(left).concat(equals).concat(quickSort(right));
 }
 
 function mergeSort(array) {
@@ -44,11 +79,19 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length === 1) return array;
+  let middle = Math.floor(array.length / 2);
+  let left = mergeSort(array.slice(0, middle));
+  let right = mergeSort(array.slice(middle));
+  let arr = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) arr.push(left.shift());
+    else arr.push(right.shift());
+  }
+  if (left.length) arr = arr.concat(left);
+  else arr = arr.concat(right);
+  return arr;
 }
-
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
